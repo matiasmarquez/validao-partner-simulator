ff500519efb2a515a14aeb1b95a560eaca79941f
Matias <matias@trovatrip.com>
2025-01-28 16:50:31 -0300

    updated sdk trigger

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,34 @@
 export {};
 
+type SDKTheme = "light" | "dark";
+type SDKRounded = "none" | "md" | "lg";
+type SDKFont = "inconsolata" | "outfit";
+type SDKColor = "red" | "sky" | "teal";
+
+type SDKCustomization = {
+  theme?: SDKTheme;
+  rounded?: SDKRounded;
+  font?: SDKFont;
+  color?: SDKColor;
+};
+
+type ShowMessageFn = (message: string) => void;
+
+type SDKOnStakeOpts = {
+  closeModal: () => void;
+  showSuccessMessage: ShowMessageFn;
+};
+
+type SDKOnUnstakeOpts = {
+  closeModal: () => void;
+  showSuccessMessage: ShowMessageFn;
+};
+
+type SDKOnErrorOpts = {
+  closeModal: () => void;
+  showErrorMessage: ShowMessageFn;
+};
+
 type OnStakeOpts = {
   closeModal: () => void;
   showSuccessMessage: (message: string) => void;
@@ -12,16 +41,16 @@ type OnErrorOpts = {
 
 export interface StakeModalConfig {
   chain: string;
-  title?: string;
-  onStake?: (opts: OnStakeOpts) => void;
-  onError?: (error: string, opts: OnErrorOpts) => void;
+  onStake?: (opts: SDKOnStakeOpts) => void;
+  onUnstake?: (opts: SDKOnUnstakeOpts) => void;
+  onError?: (error: string, opts: SDKOnErrorOpts) => void;
+  customization?: SDKCustomization;
 }
 
 declare global {
   interface Window {
     validao: {
       openStakeModal: (config: StakeModalConfig) => void;
-      openDummyStakeModal: (config: StakeModalConfig) => void;
     };
   }
 }
